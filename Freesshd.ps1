3d7ae8fe8af16b4712a7d2f0b434ee7881bb3825
unknown <lymgmxd@163.com>
2020-05-23 20:36:31 +0800

    COmfirm

--- Freesshd.ps1
+++ Freesshd.ps1
@@ -1,8 +1,18 @@
-# 20200520V1
+# 20200523V1
 # author mengxd
+$status = 1 # 0:need transfer file 1: No need transfer file
 $path = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $logfile = $path + "\psscript.log" # log file in local pc
-$status = 1 # 0:transfer file 1: No transfer file
+# update script
+try {
+    $client = New-Object System.Net.WebClient
+    $client.Credentials = New-Object System.N et.NetworkCredential("administrator", "tiens_123")
+    $client.DownloadFile("ftp://192.168.2.237/freesshdlog/Freesshd.ps1", $path + "\Freesshd.ps1")
+    "$(get-date)" + " Upgrade script succeeded." | Out-File -Append $logfile -Encoding UTF8
+}
+catch {
+    "$(get-date)" + " Upgrade script failed." | Out-File -Append $logfile -Encoding UTF8
+}
 if ((Get-Service *FreeSSHDService* | Select-Object DisplayName).DisplayName -eq "FreeSSHDService") {
     try {
         $localaddr = (ipconfig.exe | findstr "IPv4").split(":")[1].Trim(" .-`t`n`r")
@@ -13,7 +23,7 @@ if ((Get-Service *FreeSSHDService* | Select-Object DisplayName).DisplayName -eq 
     }
     $result = netstat -ano | findstr 0.0.0.0:22
     if ($result -like "*TCP*") {
-        "$(get-date)" + " FreeSSHDservice Is running..." | Out-File -Append $logfile -Encoding UTF8
+        # "$(get-date)" + " FreeSSHDservice Is running..." | Out-File -Append $logfile -Encoding UTF8
         $status = 0 # FreeSSHDservice is working fine, no need to restart service.
     }
     else {
@@ -35,7 +45,7 @@ if ($status -eq 1) {
     try {
         $client = New-Object System.Net.WebClient
         $client.Credentials = New-Object System.Net.NetworkCredential("administrator", "tiens_123")
-        $client.UploadFile("ftp://192.168.2.237/freesshdlog/"+ $localaddr+".log", $logfile)
+        $client.UploadFile("ftp://192.168.2.237/freesshdlog/" + $localaddr + ".log", $logfile)
     }
     catch {
         "$(get-date)" + " Connect to FTP server failed." | Out-File -Append $logfile -Encoding UTF8
